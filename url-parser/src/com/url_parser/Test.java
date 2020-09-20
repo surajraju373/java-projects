ce73e8ac6bc9ff4697ca7a033ef210a5caf95244
surajraju.g <surajraju.g@servicenow.com>
2020-09-20 10:16:18 +0530

    testing

--- url-parser/src/com/url_parser/Test.java
+++ url-parser/src/com/url_parser/Test.java
@@ -13,10 +13,11 @@ public class Test {
 	         System.out.println("URL is " + url.toString());
 
 	         System.out.println("protocol is " + url.getProtocol());
-	         
+
 	         System.out.println("authority is " + url.getAuthority());
 	         System.out.println("file name is " + url.getFile());
 	         System.out.println("host is " + url.getHost());
+	         
 	         System.out.println("path is " + url.getPath());
 	         System.out.println("port is " + url.getPort());
 	         System.out.println("default port is " + url.getDefaultPort());
